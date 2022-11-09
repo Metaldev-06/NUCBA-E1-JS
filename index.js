8451d77f2debbf726b64d6b6069af181a1047a53
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-11-09 18:22:08 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const Pizzas = [
+  {
+    id: 1,
+    name: "napolitana",
+    price: 1500,
+    ingredients: [
+      "Queso Mozzarella",
+      "Tomatitos Cherry",
+      "Albahaca",
+      "Orégano",
+    ],
+  },
+  {
+    id: 2,
+    name: "4 quesos",
+    price: 500,
+    ingredients: [
+      "Salsa de tomate",
+      "Queso Fontina",
+      "Queso Mozzarela",
+      "Queso Parmigiano Reggiano",
+      "Queso Gorgonzola",
+      "Orégano",
+      "Pimienta",
+    ],
+  },
+  {
+    id: 3,
+    name: "Pizza hawaiana",
+    price: 10,
+    ingredients: [
+      "Salsa de tomate",
+      "Piña",
+      "Jamón cocido",
+      "Mozzarella rallado",
+      "Orégano",
+    ],
+  },
+  {
+    id: 4,
+    name: "Pizza de chorizo",
+    price: 800,
+    ingredients: [
+      "Salsa de tomate",
+      "chorizo",
+      "Queso Mozzarela",
+      "Perejil",
+      "Orégano",
+    ],
+  },
+  {
+    id: 5,
+    name: "Pizza de jamón y champiñones",
+    price: 1500,
+    ingredients: [
+      "Salsa de tomate",
+      "Jamón cocido",
+      "Champiñones",
+      "Queso Mozzarela",
+      "Orégano",
+    ],
+  },
+  {
+    id: 6,
+    name: "Pizza de jamón y morrones",
+    price: 1000,
+    ingredients: [
+      "Salsa de tomate",
+      "Jamón cocido",
+      "Morrones",
+      "Queso Mozzarela",
+      "Orégano",
+    ],
+  },
+];
+
+console.warn("--------------------A------------------------");
+
+Pizzas.forEach((pizza) => {
+  if (!(pizza.id % 2) == 0) {
+    console.log(`El precio de la pizza ${pizza.name} es impar`);
+  }
+});
+
+console.warn("--------------------B------------------------");
+
+Pizzas.forEach((pizza) => {
+  if (pizza.price < 600) {
+    console.log(
+      `La pizza ${pizza.name} tiene un precio menor a $600, su precio es de $${pizza.price}`
+    );
+  }
+});
+
+console.warn("--------------------C------------------------");
+
+Pizzas.forEach((pizza) => {
+  console.log(`La pizza ${pizza.name} tiene un precio de $${pizza.price}`);
+});
+
+console.warn("--------------------D------------------------");
+
+Pizzas.forEach((pizza) => {
+  console.log(`Pizza ${pizza.name} - Ingredientes: \n${pizza.ingredients}`);
+});
